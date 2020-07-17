67b3ab686f41f80f4a297d78dc5d98364237c9a8
Mary Baker <meb601@gmail.com>
2020-07-16 23:03:34 -0400

    Complete Stretch 2

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ console.log(getLastReview(reviews));
 */
 
  function getReviewByRating(reviewsArray, reviewRating) {
-   let ratingArray = []
+   let ratingArray = [];
     for (i = 0; i < reviewsArray.length; i++) {
       let reviewRatingAtIndex = reviewsArray[i].rating;
       reviewRatingAtIndex = Math.floor(reviewRatingAtIndex);
@@ -143,10 +143,18 @@ and should return an array of objects.
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." }]
 */
-  function getLongReviews(/* code here */) {
-    /* code here */
+  function getLongReviews(reviewsArray) {
+    let longestReviewsArray = [];
+    for(i = 0; i < reviewsArray.length; i++) {
+      let reviewSentence = reviewsArray[i].feedback;
+      reviewSentence = reviewSentence.split(" ");
+      if (reviewSentence.length > 15) {
+        longestReviewsArray.push(reviewsArray[i].feedback)
+      }
+    }
+    return longestReviewsArray;
   }
-  
+  console.log(getLongReviews(reviews));
 
 /* STRETCH 3:  This challenge is not related to the data above! 
 
